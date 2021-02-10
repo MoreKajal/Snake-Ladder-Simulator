e9304f9dc6d3e7c0b77f1dddf7c5562022d527fe
MoreKajal <kajalm3105@gmail.com>
2021-02-10 08:24:17 -0800

    [Kajal]Added:Player Rolled The Dice Code

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,9 @@ public class SnakeLadder {
         public static void main(String[] args) {
                 int player_position = 0;
                 System.out.println("***Welcome To Snake N Ladder Game***");
+		double roll_dice = Math.floor(Math.random() * 10 ) % 6 + 1;
+                System.out.println(" Dice Rolled : " + roll_dice);
+
         }
 }
 
