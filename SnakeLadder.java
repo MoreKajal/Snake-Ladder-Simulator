fc3f297b06519c132060f14d18fe67e278cb1973
MoreKajal <kajalm3105@gmail.com>
2021-02-10 08:15:44 -0800

    [Kajal]Added:Starting Position 0 code

---
+++ SnakeLadder.java
@@ -0,0 +1,7 @@
+public class SnakeLadder {
+        public static void main(String[] args) {
+                int player_position = 0;
+                System.out.println("***Welcome To Snake N Ladder Game***");
+        }
+}
+
