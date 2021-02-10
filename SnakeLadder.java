55598a2e115a1edb0d3d6dfa0c15e80b4aff43e4
Kajal-More <56627613+MoreKajal@users.noreply.github.com>
2021-02-10 02:29:40 -0800

    [Kajal]:Refactored player check for an option code

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,6 @@
 public class SnakeLadder {
         public static final int IS_LADDER = 1;
         public static final int IS_SNAKE = 2;
-
         public static void main(String[] args) {
                 int player_position = 0;
                 System.out.println("***Welcome To Snake N Ladder Game***");
@@ -10,7 +9,6 @@ public class SnakeLadder {
                 System.out.println(" Dice Rolled : " + roll_dice);
                 double check_option = Math.floor(Math.random() * 10 ) % 3;
                 System.out.println("The Checks The Option : " + check_option);
-
                 switch ((int)check_option) {
                         case IS_LADDER:
                                 player_position+=roll_dice;
